20b7a6b34ee8df924fd49ae27c8944b3f5ed14ba
Bradley Ouellette <bradou@ouellettetech.com>
2021-01-25 16:43:25 -0800

    Adding Package option

--- build.sh
+++ build.sh
@@ -11,6 +11,11 @@ ssh-keygen -F github.com || ssh-keyscan github.com >>~/.ssh/known_hosts
 printf "> Starting build\n"
 sh scripts/onion-feed-setup.sh
 git checkout .config
+if [[ ! -v PACKAGE_CHECKOUT ]]; then
+    pushd feeds/oboo
+    git checkout PACKAGE_CHECKOUT
+    popd
+fi
 
 # build
 make -j 3
