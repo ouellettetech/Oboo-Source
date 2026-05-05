bd530662058bf47c47f2f0879f2b8b6f81fa33a1
Bradley Ouellette <bradou@ouellettetech.com>
2026-05-05 11:49:20 -0400

    Update onion-feed-setup.sh
    Trying to make sure ca certs are in the env....

--- scripts/onion-feed-setup.sh
+++ scripts/onion-feed-setup.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 
 echo "Preparing package feeds..."
+git config --list | grep ssl
+sudo apt-get install -y ca-certificates
+git config --list | grep ssl
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 #./scripts/feeds install -a -p onion
