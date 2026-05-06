35646f3928418dc76051e9b0ff840bc36054f6c0
Bradley Ouellette <bradou@ouellettetech.com>
2026-05-06 15:19:23 -0400

    Update onion-feed-setup.sh
    Trying to add to git config....

--- scripts/onion-feed-setup.sh
+++ scripts/onion-feed-setup.sh
@@ -1,10 +1,19 @@
 #! /bin/sh
 
 echo "Preparing package feeds..."
+
 git config --list | grep ssl || true
+
+echo "Installing CA certificates..."
+sudo apt-get update -y
 sudo apt-get install -y ca-certificates
-git config --list | grep ssl || true
+
+echo "Configuring git to use CA bundle..."
+git config --global http.sslCAinfo /etc/ssl/certs/ca-certificates.crt
+
 echo "After setup of ca certificates..."
+git config --list | grep ssl || true
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 #./scripts/feeds install -a -p onion
