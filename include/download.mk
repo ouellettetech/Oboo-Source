7c764ff44fb1b5be2369c561413ca3678fd2737e
Bradley E. Ouellette <bradou@ouellettetech.com>
2026-05-08 20:03:25 -0400

    pointing reference files to github mirror

--- include/download.mk
+++ include/download.mk
@@ -7,7 +7,7 @@
 #
 
 OPENWRT_GIT = http://git.openwrt.org
-LEDE_GIT = https://git.lede-project.org
+GITHUB_MIRROR = https://github.com/openwrt
 
 ifdef PKG_SOURCE_VERSION
 PKG_VERSION ?= $(if $(PKG_SOURCE_DATE),$(PKG_SOURCE_DATE)-)$(call version_abbrev,$(PKG_SOURCE_VERSION))
